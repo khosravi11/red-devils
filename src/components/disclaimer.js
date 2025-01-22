2f1bc901ed50f5fe80462f1b881b0053f1acc0da
khosravi11 <khosravi.saam@gmail.com>
2025-01-21 18:30:04 -0800

    Add affiliates section

---
+++ src/components/disclaimer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Team = () => {
+    return (
+        <div id='disclaimer' className='bg-red py-4'>
+            <div className='w-100 text-center'>
+                <p className='text-white'>For more info Call Coach Scott: (510) 365-8568 </p>
+            </div>
+        </div>
+    );
+};
+
+export default Team;
